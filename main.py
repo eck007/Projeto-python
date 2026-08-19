cea787f190acbb34c6a8c561c9dadd00a8d23c58
Erick Ripari Gomes <erickgomes@itsegcompany.onmicrosoft.com>
2026-08-19 16:01:38 -0300

    tratamento de erros nas receitas

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-#   PARTE 1 MENU INTERATIVO 
+#   PARTE MENU INTERATIVO 
 print("    FinanceIA   ")
 
 opcao=""
 receitas = []
 despesas = []
+
 while opcao != "5":
  print("\n1 - Adicionar receita")
  print("2 - Adicionar despesa")
@@ -14,42 +15,65 @@ while opcao != "5":
  opcao = input("Escolha uma opçao:")
  print("Você escolheu:", opcao)
 
- #   PARTE 2 VALORES
+ #   PARTE VALORES
  if opcao == "1":
     valor = float(input("Digite o valor da receita: R$").replace(",","."))
-    receitas.append(valor)
+    descricao = input("digite a descrição da receita:")
+
+    receitas.append({"valor": valor,
+                     "descricao": descricao})
+
     print(f"Receita de R$ {valor:.2f} adicionada!")
 
  elif opcao == "2":
     valor = float(input("Digite o valor da despesa: R$").replace(",","."))
-    despesas.append(valor)
+    descricao = input("Digite a descrição da despesa:")
+    categoria = input("digite a categoria da despesa:")
+
+    despesas.append({ "valor": valor,"descricao":descricao,"categoria":categoria})
+    
     print(f"Despesa de R$ {valor:.2f} adicionada!")
 
-#   PARTE 4 SALDO
+#   PARTE  SALDO
  elif opcao == "3":
-    total_receitas = sum(receitas)
-    total_despesas = sum(despesas)
+    total_receitas = 0
+
+    for receita in receitas:
+       total_receitas += receita["valor"]
+
+       total_despesas = 0
+    for despesa in despesas:
+       total_despesas += despesa["valor"]
 
     saldo = total_receitas - total_despesas
 
     print(f"\nSaldo atual: R$ {saldo:.2f}")
 
-#   PARTE 3 TRANSAÇÔES5
+#   PARTE  TRANSAÇÔES5
  elif opcao == "4":
     print("\n     TRANSAÇÔES     ")
 
     print("Receitas:")
 
     for receita in receitas:
-       print(f"R$ {receita:.2f}")
+       print(f"R$ {receita['valor']:.2f} - {receita['descricao']}")
        
-    total_receitas = sum(receitas)
+    total_receitas = 0
+    for receita in receitas:
+       total_receitas +=receita["valor"]
+
     print(f"\nTotal de receitas: R$ {total_receitas:.2f}")
     print("\nDespesas")
 
     for despesa in despesas:
-       print(f"R$ {despesa:.2f}")
-    total_despesas = sum(despesas)
+       print(f"R$ {despesa['valor']:.2f} - "
+             f"{despesa['descricao']}"
+             f"({despesa['categoria']})")
+
+    total_despesas = 0
+    for despesa in despesas:
+       total_despesas += despesa["valor"]
+
     print(f"\nTotal de despesas: R$ {total_despesas:.2f}")
     
  elif opcao == "5":
